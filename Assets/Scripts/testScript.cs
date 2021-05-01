376aa9b81a0b2dc05036b1ae5125eadb6fcdebb3
Tengis <chinzorigtengis@gmail.com>
2021-05-01 10:10:42 +0200

    Before defining kinematics

---
+++ Assets/Scripts/testScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    public float startDeg, endDeg, speed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Horizontal"))
+        {
+            gameObject.transform.Rotate(Vector3.up ,Time.deltaTime*speed);
+        }
+        else if (Input.GetButton("Vertical"))
+        {
+            gameObject.transform.Rotate(Vector3.up ,-Time.deltaTime*speed);
+        }
+    }
+}
